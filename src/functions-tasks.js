15f01148f155863675ae33c713d91dd1b95d6650
onqqr <dnbyaniss@gmail.com>
2024-04-03 12:46:04 +0300

    feat: solved partialUsingArguments

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -183,8 +183,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => {
+    return fn.apply(this, [...args1, ...args2]);
+  };
 }
 
 /**
